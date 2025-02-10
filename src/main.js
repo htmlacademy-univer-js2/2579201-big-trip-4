096b9ebced6bd85783b6948f76607ddde51ce4f0
Anton <79142916+skyfxllen@users.noreply.github.com>
2025-02-10 23:14:46 +0700

    presenter fixes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-import FilterView from './view/filters-view.js';
-import {render} from './render.js';
 import Presenter from './presenter/presenter.js';
 
-const siteMainElement = document.querySelector('.trip-events');
-const siteFilterElement = document.querySelector('.trip-controls__filters');
-const presenter = new Presenter({container: siteMainElement});
+const presenter = new Presenter();
 
-render(new FilterView(), siteFilterElement);
 presenter.init();
